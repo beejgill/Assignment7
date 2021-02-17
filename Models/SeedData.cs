c2ca05503d7f62ff4444f07617122dbc9fec11ca
beejgill <beejgill@gmail.com>
2021-02-17 11:16:02 -0700

    Add project files.

---
+++ Models/SeedData.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated (IApplicationBuilder application)
+        {
+            BookDbContext context = application.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<BookDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if(!context.Books.Any())
+            {
+                context.Books.AddRange(
+
+                    new Book
+                    {
+                        BookTitle = "Les Miserables",
+                        AuthorFirstName = "Victor",
+                        AuthorLastName = "Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419439",
+                        Classification = "Fiction",
+                        Category = "Classic",
+                        Price = 9.95F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "Team of Rivals",
+                        AuthorFirstName = "Doris Kearns",
+                        AuthorLastName = "Goodwin",
+                        Publisher = "Simon and Schuster",
+                        ISBN = "978-0743270755",
+                        Classification = "Non-Fiction",
+                        Category = "Biography",
+                        Price = 14.58F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "The Snowball",
+                        AuthorFirstName = "Alice",
+                        AuthorLastName = "Schroeder",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553384611",
+                        Classification = "Non-Fiction",
+                        Category = "Biography",
+                        Price = 21.54F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "American Ulysses",
+                        AuthorFirstName = "Ronald C.",
+                        AuthorLastName = "White",
+                        Publisher = "Random House",
+                        ISBN = "978-0812981254",
+                        Classification = "Non-Fiction",
+                        Category = "Biography",
+                        Price = 11.61F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "Unbroken",
+                        AuthorFirstName = "Laura Hillenbrand",
+                        AuthorLastName = "Hillenbrand",
+                        Publisher = "Random House",
+                        ISBN = "978-0812974492",
+                        Classification = "Non-Fiction",
+                        Category = "Historical",
+                        Price = 13.33F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "The Great Train Robbery",
+                        AuthorFirstName = "Michael",
+                        AuthorLastName = "Crichton",
+                        Publisher = "Vintage",
+                        ISBN = "978-0804171281",
+                        Classification = "Fiction",
+                        Category = "Historical Fiction",
+                        Price = 15.95F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "Deep Work",
+                        AuthorFirstName = "Cal",
+                        AuthorLastName = "Newport",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455586691",
+                        Classification = "Non-Fiction",
+                        Category = "Self-Help",
+                        Price = 14.99F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "It's Your Ship",
+                        AuthorFirstName = "Michael",
+                        AuthorLastName = "Abrashoff",
+                        Publisher = "Grand Central Publishing",
+                        ISBN = "978-1455523023",
+                        Classification = "Non-Fiction",
+                        Category = "Self-Help",
+                        Price = 21.66F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "The Virgin Way",
+                        AuthorFirstName = "Richard",
+                        AuthorLastName = "Branson",
+                        Publisher = "Portfolio",
+                        ISBN = "978-1591847984",
+                        Classification = "Non-Fiction",
+                        Category = "Business",
+                        Price = 29.16F
+                    },
+
+                    new Book
+                    {
+                        BookTitle = "Sycamore Row",
+                        AuthorFirstName = "John",
+                        AuthorLastName = "Grisham",
+                        Publisher = "Bantam",
+                        ISBN = "978-0553393613",
+                        Classification = "Fiction",
+                        Category = "Thrillers",
+                        Price = 15.03F
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
